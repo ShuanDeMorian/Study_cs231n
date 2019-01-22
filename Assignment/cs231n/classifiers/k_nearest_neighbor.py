54d417467e19f575aad43c7e31e31a4a07c6864d
ShuanDeMorian <38206262+ShuanDeMorian@users.noreply.github.com>
2019-01-22 13:03:09 +0900

    폴더 이름 변경 utils -> cs231n

--- Assignment/cs231n/classifiers/k_nearest_neighbor.py
+++ Assignment/cs231n/classifiers/k_nearest_neighbor.py
@@ -71,7 +71,9 @@ class KNearestNeighbor(object):
         # training point, and store the result in dists[i, j]. You should   #
         # not use a loop over dimension.                                    #
         #####################################################################
-        pass
+        t = X[i]-self.X_train[j]
+        t = t * t.transpose()
+        dists[i][j] = np.sqrt(t.sum())
         #####################################################################
         #                       END OF YOUR CODE                            #
         #####################################################################
